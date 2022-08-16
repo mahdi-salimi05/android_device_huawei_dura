7a3b39598ec1281dbd998bb0090800212fe1b352
AldairSoraki <52483168+AldairbotHerotaku@users.noreply.github.com>
2022-08-16 04:07:48 +0000

    Correct lineageOS to Lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineageos_dura-user
-add_lunch_combo lineageos_dura-userdebug
-add_lunch_combo lineageos_dura-eng
+add_lunch_combo lineage_dura-user
+add_lunch_combo lineage_dura-userdebug
+add_lunch_combo lineage_dura-eng

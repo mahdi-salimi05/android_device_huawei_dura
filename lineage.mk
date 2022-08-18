b9f0b424865abcd457ffb5f01ff370c7f99d96ad
AldairSoraki <52483168+AldairbotHerotaku@users.noreply.github.com>
2022-08-18 06:29:59 +0000

    Inherit core64

--- lineage.mk
+++ lineage.mk
@@ -4,6 +4,7 @@
 PRODUCT_RELEASE_NAME := dura
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
